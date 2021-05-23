9a05f2799d2697133a02972d438d06cb019cf39e
COW-sudo <83543575+COW-sudo@users.noreply.github.com>
2021-05-23 22:24:13 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,79 +9,40 @@ Webcam Image Classification using a pre-trained customized model and p5.js
 This example uses p5 preload function to create the classifier
 === */
 
-// Classifier Variable
+// Global variable to store the classifier
 let classifier;
-// Model URL
-let imageModelURL = 'https://cow-sudo.github.io/aifrog/';
 
-// Video
-let video;
-let flippedVideo;
-// To store the classification
-let label = "";
+// Label (start by showing listening)
+let label = "listening";
 
-// Load the model first
+// Teachable Machine model URL:
+let soundModelURL = 'https://cow-sudo.github.io/aifrog/';
+
+
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  // Load the model
+  classifier = ml5.soundClassifier(soundModelURL);
 }
 
 function setup() {
-  createCanvas(320, 320);
-  // Create the video
-  var constraints = {
-
-    audio: false,
-
-    video: {
-
-      facingMode: {
-
-        exact: "environment"
-
-      }
-
-    }   
-
-    //video: {
-
-      //facingMode: "user"
-
-    //}
-
-  };
-  
-  
-  
-  video = createCapture(constraints);
-  video.size(320, 320);
-  video.hide();
-
-  flippedVideo = ml5.flipImage(video)
+  createCanvas(320, 240);
   // Start classifying
-  classifyVideo();
+  // The sound model will continuously listen to the microphone
+  classifier.classify(gotResult);
 }
 
 function draw() {
   background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
-
-  // Draw the label
+  // Draw the label in the canvas
   fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height / 2);
 }
 
-// Get a prediction for the current video frame
-function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
-  classifier.classify(flippedVideo, gotResult);
-}
 
-// When we get a result
+// The model recognizing a sound will trigger this event
 function gotResult(error, results) {
-  // If there is an error
   if (error) {
     console.error(error);
     return;
@@ -89,7 +50,6 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-  ThunkableWebviewerExtension.postMessage(label);;
-  // Classifiy again!
-  classifyVideo();
+  ThunkableWebviewerExtension.postMessage(label);
+  
 }
